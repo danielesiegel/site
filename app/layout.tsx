01b1976c2b6e0a9da1659bcec068da1fac3bb9a5
danielesiegel <danieledisonsiegel@gmail.com>
2024-11-19 23:00:44 -0500

    some other changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import { Analytics } from '@vercel/analytics/react';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  metadataBase: new URL('https://siegel.bio'),
+  metadataBase: new URL('https://siegel.vercel.app'),
   alternates: {
     canonical: '/',
   },
