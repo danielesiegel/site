5f9ab125e4495b2365c51db12ecc06f970dcf35c
danielesiegel <danieledisonsiegel@gmail.com>
2025-04-17 10:42:26 -0400

    capitalization error

--- app/ProjectContext.tsx
+++ app/ProjectContext.tsx
@@ -21,7 +21,7 @@ export const projects: ProjectData[] = [
   { id: 'cane', title: 'Blind Cane', href: '/n/surround', imageSrc: '/images/1724364654135.jpeg' },
   { id: 'gym', title: 'Gym Optimizer', href: 'https://devpost.com/software/gymbo', imageSrc: 'https://d112y698adiu2z.cloudfront.net/photos/production/software_photos/003/291/459/datas/original.jpeg' },
   { id: 'prosthetics', title: 'Prosthetic Prototypes', href: '/n/prosthetics', imageSrc: '/images/IMG_9547.jpg' },
-  { id: 'sawtooth', title: 'Sawtooth Soundsystem', href: 'https://www.instagram.com/sawtoothsoundsystem/', imageSrc: '/images/img_8801.jpg' },
+  { id: 'sawtooth', title: 'Sawtooth Soundsystem', href: 'https://www.instagram.com/sawtoothsoundsystem/', imageSrc: '/images/IMG_8801.jpg' },
   { id: '3dprint', title: '3D Print Farm Tracker', href: '/n/3dprint', imageSrc: '/images/image (17).png' },
   { id: 'manipulators', title: 'Axial and Rotational Manipulators', href: '/n/pyramidal', imageSrc: '/images/IMG_3805.png' },
   { id: 'printfarm', title: 'Home 3D Print Farm + Lab', href: '/n/homeprintfarm', imageSrc: '/images/image (20).png' },
